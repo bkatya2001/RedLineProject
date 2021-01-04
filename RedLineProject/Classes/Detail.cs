80ed17f59cdc87ae6f5ac2f4c95a300da11f266f
Ekaterina Bolshakova <ekaterinabolshakova2001@yandex.ru>
2021-01-04 22:03:19 +0300

    Init commit

---
+++ RedLineProject/Classes/Detail.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedLineProject.Classes
+{
+    class Detail // Класс коллекции детали одного вида
+    {
+        int length;
+        int width;
+        int count;
+
+        public Detail(int _length, int _width, int _count)
+        {
+            length = _length;
+            width = _width;
+            count = _count;
+        }
+
+        public int getLength() { return length; }
+        public int getWidth() { return width; }
+        public int getCount() { return count; }
+
+        public void increment() { count++; }
+
+        public void decrement() { count--; }
+    }
+}
