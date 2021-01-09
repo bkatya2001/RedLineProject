d44b2455c1b2c27c6e47c31741484a1a32e9e407
Ekaterina Bolshakova <ekaterinabolshakova2001@yandex.ru>
2021-01-09 22:47:55 +0300

    edge added

--- RedLineProject/Form1.Designer.cs
+++ RedLineProject/Form1.Designer.cs
@@ -400,11 +400,13 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
             this.ClientSize = new System.Drawing.Size(975, 923);
             this.Controls.Add(this.tableLayoutPanel1);
             this.Controls.Add(this.materialFlatButton1);
             this.Controls.Add(this.flowLayoutPanel1);
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "RedLine";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.flowLayoutPanel1.ResumeLayout(false);
